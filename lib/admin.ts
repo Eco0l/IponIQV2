f8410e9cf4f573a03b36bd3f6a639d6bc9d90b69
Enrico <ecobautista555@gmail.com>
2024-11-30 06:39:35 +0800

    given admin to members

--- lib/admin.ts
+++ lib/admin.ts
@@ -2,7 +2,7 @@ import { auth } from "@clerk/nextjs/server"
 
 
 const adminIds =[
-    "user_2nw9FM0VtK0bHYXUxNNOnREavIb",
+    "user_2nw9FM0VtK0bHYXUxNNOnREavIb", "user_2pWp7hU72McnLHxMZr6oVVa7khC", "user_2o3hRKWlc6VltW1GXLHHfgf6Gdo", "user_2nzt56F5Z7I5wxVcay6RkZxlxfH"
 ];
 export const IsAdmin = async () =>{
     const { userId } = auth();
